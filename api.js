a25e1413e62fb428af8933a2d56e39c869b9c819
neil-feris <neil.feris@gmail.com>
2022-10-20 21:18:08 +0200

    initial commit

---
+++ api.js
@@ -0,0 +1,37 @@
+// api.js contains the api route logic
+const express = require("express");
+const router = express.Router();
+
+// import baseURL from config
+const { baseURL } = require("./config/config.json");
+
+let query = "";
+
+// a blank GET request to the api route returns a message
+router.get("/", (req, res) => {
+  if (Object.keys(req.query).length === 0) {
+    // if no query parameters are passed, return a status 400 (Bad Request) and a message
+    res.status(400).send("No query parameters passed");
+  } else {
+    // if the request contains a query, it is stored in the query variable
+    query = req.query;
+
+    // build the query string by looping through the query object
+    let queryString = "";
+    for (let key in query) {
+      queryString += `${key}=${query[key]}&`;
+    }
+    // strip the trailing ampersand
+    queryString = queryString.slice(0, -1);
+
+    // Make a fetch request to the baseURL with the query and store the result in the result variable
+    fetch(`${baseURL}&${queryString}`)
+      .then((res) => res.json())
+      .then((result) => {
+        res.send(result);
+      });
+  }
+});
+
+// export API router
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,24 @@
+// import express
+const express = require("express");
+const app = express();
+
+// import helmet
+const helmet = require("helmet");
+app.use(helmet());
+
+// import api router
+const api = require("./api");
+
+// Set up api route
+app.use("/api", api);
+
+// read port from environment variable or use 3001
+const PORT = process.env.PORT || 3001;
+
+// Set up basic response
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// start the express web server listening on 3001
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
