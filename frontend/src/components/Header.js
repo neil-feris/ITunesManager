0ac0f099c69551f07b30d0295d78367f78d8b856
neil-feris <neil.feris@gmail.com>
2022-10-22 21:36:20 +0200

    remove mui from backend

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,19 @@
 // Header.js renders the header bar of the App
 import React from "react";
 
-const Header = () => {
+import { AppBar, Toolbar, Typography } from "@mui/material";
+
+function Header() {
   return (
-    <div className="header">
-      <h1>My App</h1>
-    </div>
+    <AppBar position="static">
+      {/* position="static" means the AppBar will be fixed to the top of the page */}
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          ITunes Manager
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
-};
+}
 
 export default Header;
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,12 +1,70 @@
-// Main.js renders the main area of the app
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Main = () => {
-  return (
-    <div className="main">
-      <h1>Main</h1>
-    </div>
-  );
-};
+import {
+  Box,
+  Container,
+  Grid,
+  Typography,
+  FormControl,
+  FormGroup,
+  Input,
+  Select,
+  TextField,
+} from "@mui/material";
+
+function Main() {
+  const mediaTypes = [
+    { movie: "Movie" },
+    { podcast: "Podcast" },
+    { music: "Music" },
+    { musicVideo: "Music Video" },
+    { audiobook: "Audiobook" },
+    { shortFilm: "Short Film" },
+    { tvShow: "TV Show" },
+    { software: "Software" },
+    { ebook: "Ebook" },
+    { all: "All" },
+  ];
+
+  const searchTypes = [
+    { movie: [{ movieArtist: "Artist" }, { movie: "Movie" }] },
+    { podcast: [{ podcastAuthor: "Author" }, { podcast: "Podcast" }] },
+    {
+      music: [
+        { musicArtist: "Artist" },
+        { musicTrack: "Music Track" },
+        { album: "Album" },
+        { musicVideo: "Music Video" },
+        { mix: "Mix" },
+        { song: "Song" },
+      ],
+    },
+    { musicVideo: [{ musicArtist: "Artist" }, { musicVideo: "Music Video" }] },
+    { audiobook: [{ audiobookAuthor: "Author" }, { audiobook: "Audiobook" }] },
+    { shortFilm: [{ shortFilmArtist: "Artist" }, { shortFilm: "Short Film" }] },
+    { tvShow: [{ tvEpisode: "Episode" }, { tvSeason: "Season" }] },
+    {
+      software: [
+        { software: "Software" },
+        { iPadSoftware: "Ipad Software" },
+        { macSoftware: "Mac Software" },
+      ],
+    },
+    { ebook: [{ ebook: "Ebook" }] },
+    {
+      all: [
+        { movie: "Movie" },
+        { album: "Album" },
+        { allArtist: "Artist" },
+        { podcast: "Podcast" },
+        { musicVideo: "Music Video" },
+        { mix: "Mix" },
+        { audiobook: "Audiobook" },
+        { tvSeason: "TV Season" },
+        { allTrack: "Track" },
+      ],
+    },
+  ];
+}
 
 export default Main;
