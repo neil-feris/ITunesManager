745a3a9542196ef70dd5089cd12bfbdf7ba70231
neil-feris <neil.feris@gmail.com>
2022-10-28 11:29:53 +0200

    change start script

--- server.js
+++ server.js
@@ -20,5 +20,12 @@ app.get("/", (req, res) => {
   res.send("Itunes API!");
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
 // start the express web server listening on 3001
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
